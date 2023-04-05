510a7b19164f03307d76c555004fbe8a91e66f2c
donguramee <sanulim94@gmail.com>
2023-04-05 23:03:40 +0900

    검색바 수정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -22,4 +23,26 @@ function showBasket() {
 }
 function hideBasket() {
   basketEl.classList.remove("show");
+}
+
+// 검색
+const headerEl = document.querySelector("header");
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+
+searchStarterEl.addEventListener("click", showSearch);
+
+searchCloserEl.addEventListener("click", hideSearch);
+
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+}
+function hideSearch() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
 }

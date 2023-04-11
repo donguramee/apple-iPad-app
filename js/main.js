0120c8178a7664c80dc04a0ecd46f906e8809ad7
donguramee <sanulim94@gmail.com>
2023-04-11 23:44:17 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import ipads from "../data/ipads.js";
+import navigations from "../data/navigations.js";
+
 // 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
@@ -93,4 +96,54 @@ pauseBtn.addEventListener("click", function () {
   video.pause();
   playBtn.classList.remove("hide");
   pauseBtn.classList.add("hide");
+});
+
+// '당신에게 맞는 아이패드는 ?' 렌더링!
+const itemsEl = document.querySelector("section.compare .items");
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`;
+  });
+
+  itemEl.innerHTML = /*html*/ `
+  <div class="thumbnail">
+    <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+  </div>
+  <ul class="colors">
+    ${colorList}
+  </ul>
+  <h3 class="name">${ipad.name}</h3>
+  <p class="tagline">${ipad.tagline}</p>
+  <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+  <button class="btn">구입하기</button>
+  <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+  itemsEl.append(itemEl);
+});
+
+const navigationsEl = document.querySelector("footer .navigations");
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement("div");
+  mapEl.classList.add("map");
+
+  let mapList = "";
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `<li>
+    <a href="${map.url}">${map.name}</a>
+    </li>`;
+  });
+
+  mapEl.innerHTML = /* html */ `
+  <h3>
+    <span class="text">${nav.title}</span>
+  </h3>
+  <ul>
+    ${mapList}
+  </ul>
+  `;
+  navigationsEl.append(mapEl);
 });
